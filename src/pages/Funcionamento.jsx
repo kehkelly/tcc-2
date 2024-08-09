24987127ea37d2d667876af70e5348aada89f8a5
Kelly Dantas <kdsl.dantas2017@gmail.com>
2024-08-09 01:27:00 +0000

    HTML de funcionamento e sobre nós finalizado

--- src/pages/Funcionamento.jsx
+++ src/pages/Funcionamento.jsx
@@ -12,8 +12,16 @@ export function Funcionalmento() {
 
         <p>A coqueteleira será conectada ao seu smartphone via Bluetooth e por ele você pode escolher as bebidas de sua preferência para mistura-las na montagem do seu drink.</p>
         <img src="" alt="imagem de um celular conectando o bluetooth" />
+        <img src={Pontos} alt="três pontos na vertical" />
 
-        
+        <p>A comunicação direta com o celular faz com que um Led seja aceso na coqueteleira, indicando então a preparação do drink.
+        </p>
+        <img src="" alt="imagem de simulação da máquina com o led de processamento ligado" />
+        <img src={Pontos} alt="três pontos na vertical" />
+
+        <p>Após seu preparo, outro Led é aceso, indicando então o término da montagem da bebida.</p>
+        <img src="" alt="imagem de uma simulação da maquina com o led de finalização ligado" />
+
       </main>
     </body>
   )
--- src/pages/Sobre_nos.jsx
+++ src/pages/Sobre_nos.jsx
@@ -1,5 +1,33 @@
+import { Header } from "../components/Header"
+import Ponto from "../assets/pontos.svg"
+
 export function Sobre_nos() {
   return (
-    <div>Sobre_nos</div>
+    <body>
+      <Header/>
+      <main>
+        <h2>Nossa equipe</h2>
+        <p>Estudantes, técnico em informática, UNASP:</p>
+        <img src={Pontos} alt="três pontos na vertical" />
+        <div>
+          <img src="" alt="foto de uma mulher, integrante do grupo" />
+          <h3>Kelly Dantas</h3>
+          <p>Entusiasta da tecnologia, hábil em programação, web design, entre outros.</p>
+          <a href="">Linkedin</a>
+        </div>
+        <div>
+          <img src="" alt="foto de uma mulher, integrante do grupo" />
+          <h3>Leislane da Silva</h3>
+          <p>Amante de design, hábil em marketing e design.</p>
+          <a href="">Linkedin</a>
+        </div>
+        <div>
+          <img src="" alt="foto de uma mulher, integrante do grupo" />
+          <h3>Thais da Silva</h3>
+          <p>Perita na área comercial, hábil em falar e tratar as pessoas.</p>
+          <a href="">Linkedin</a>
+        </div>
+      </main>
+    </body>
   )
 }
