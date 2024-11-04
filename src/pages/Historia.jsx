c3ac510cb78ff3f6b1f33f0add8c26569f8ec4b5
Kelly Dantas <kdsl.dantas2017@gmail.com>
2024-11-04 20:41:06 +0000

    video do jogo

--- src/pages/Historia.jsx
+++ src/pages/Historia.jsx
@@ -9,25 +9,9 @@ export function Historia() {
       <main>
         <section className="sectionGeral">
           <div>
-            <h2>História</h2>
+            <h2>Jogo</h2>
           </div>
-          <p>A inspiração da criação do protótipo minimalista veio de um amigo, Miguel, que enquanto trabalhava em um bar teve uma explosão de ideias.</p>
-
-          <div>
-            <img src="/img-de-bar.jpg" alt="ilustração de um bar" className="imagem"/>
-          </div>
-
-          <p>Ele percebeu que poderia existir algo para combinar sua paixão por coquetéis com a praticidade do dia a dia, mas não sabia como e conversou com a nossa equipe.</p>
-
-          <div>
-            <img src={ Pontos } alt="três pontos na vertical" />
-          </div>
-
-          <p>Como resultado da conversa, nasce uma tecnologia verdadeiramente inovadora: uma coqueteleira inteligente.</p>
-
-          <div>
-            <img src="/img_reuniao.jpg" alt="pessoas reunidas para conversar" className="imagem"/>
-          </div>
+          <iframe width="900" height="655" src="https://www.youtube.com/embed/-6weBAixNyA?si=SLFoegdp0YMWg7j0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </section>
       </main>
       <Footer />
